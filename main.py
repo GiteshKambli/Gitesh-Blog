c63294150f726a7a85afe691c1b7358bdbdd1c67
Gitesh <90142352+GiteshKambli@users.noreply.github.com>
2022-04-15 16:25:00 +0530

    Bug fix

--- main.py
+++ main.py
@@ -198,14 +198,14 @@ def edit_post(post_id):
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
+        author=post.author.name,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
+        post.author = current_user
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
