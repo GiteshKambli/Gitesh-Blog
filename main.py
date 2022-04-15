9d2dc88170de64f81abf7d464e938b4d42f552c2
Gitesh <90142352+GiteshKambli@users.noreply.github.com>
2022-04-15 15:49:58 +0530

    Bug fix

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ db = SQLAlchemy(app)
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.String(250), db.ForeignKey('users.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author = relationship('User', back_populates='posts')
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
